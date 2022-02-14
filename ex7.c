4f1908b5a2c7e74f825c92364728fddc72bb3b94
chiyanglin-AStar <chiyanglin@gmail.com>
2022-02-14 00:47:28 +0000

    update examples

---
+++ ex7.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+    int a, b, product;
+    a = 20;
+    b = 7;
+
+    printf("a = %d \n", a);
+    printf("b = %d \n", b);
+    
+    // Calculating add 
+    product = a + b; 
+    printf("add = %d \n", product);
+
+    // Calculating add 
+    product = a - b; 
+    printf("sub = %d \n", product);
+
+    // Calculating product
+    product = a * b; 
+    printf("Product = %d \n", product);
+    
+    // Calculating div
+    product = a / b; 
+    printf("div = %d \n", product);
+
+    return 0;
+}
