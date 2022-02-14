b387f66d7cbf4503ab1d6111aa26d17e19402d86
chiyanglin-AStar <chiyanglin@gmail.com>
2022-02-14 02:23:49 +0000

    udpate

--- ex1.py
+++ ex1.py
@@ -1,2 +1,3 @@
+# this is hello 
 print (' Hello World')
 print (' my height is ', 1, 'm ', 80 , "cm")
---
+++ ex11.c
@@ -0,0 +1,5 @@
+#include<stdio.h>
+int main(){
+    int x =2 ;
+    printf(" x + 2.111 = %f \n ",x+2.111);
+}
